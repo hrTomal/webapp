00a87b0d5775d92249d4fc17a88167e29c9d809b
MD Hasibur Rahman Tomal <hr.tomal@gmail.com>
2020-12-06 09:02:34 +0600

    Add files via upload

---
+++ Final/Labtask2/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CSS Responsive</title>
+		<link rel="stylesheet" href="views/style.css">
+	</head>
+
+	<body>
+		<header id="top" class="grid">
+			<div class="headBg"></div>
+			<div class="headtext">
+			<h1>Turbo (Ryan Reynolds)-2013</h1>
+			<p>
+				Turbo (Ryan Reynolds) is a speed-obsessed snail with an unusual dream: to become the world's greatest racer. This odd snail gets a chance to leave his slow-paced life behind when a freak accident gives him the power of superspeed.
+			</p>
+			</div>
+		</header>
+		<br>
+
+		<section id="sec-a" class="grid">
+			<div>
+				<h2 class="review">Critic Reviews & Public Reactions</h2>
+				<div>
+					<p class="review_text">
+						"This is one of the best movies of the decade, showing the emotion that comes with being a human, and the vulnerability of being a man. In this thriller, we meet our young hero, Tyson. As he walks the fields of his home, he encounters a blue snail. He decides to take him home, only to release him shortly after. As he awakes in the middle of the night, he sees that snails are entering his home, only to turn him into a snail."
+					</p>
+				</div>
+			</div>
+
+		</section>
+		
+		<br>
+		<section id="sec-c" class="grid">
+			<ul>
+				<li>
+					<div class="card">
+						<img src="views/turbo1.jpg" alt="Turbo">
+						<div class="card-content">
+							<h3 class="card-title">Turbo</h3>
+							
+						</div>
+					</div>
+				</li>
+				<li>
+					<div class="card">
+						<img src="views/turbo2.jpg" alt="Turbo">
+						<h3 class="card-title">Turbo</h3>
+						
+					</div>
+					
+				</li>
+				<li>
+					<div class="card">
+						<img src="views/turbo3.jpg" alt="Turbo">
+						<h3 class="card-title">Turbo</h3>
+						
+					</div>
+					
+				</li>
+			</ul>
+			
+		</section>
+		<br>
+		<section id="sec-d" class="grid">
+			<h2 class="about">About the movie</h2>
+				<div>
+					<p class="about_text">
+						Along with his streetwise snail crew and new human friends, Turbo, an underdog snail, takes on the biggest challenge ever to make their dreams come true.
+					</p>
+		</section>
+		<br>
+		<section id="sec-e" class="grid2">
+			<div class="box">
+			<h2 class="contact-text">Contact Maker</h2>
+			<p>
+				Thanks to movies like Shrek, Trolls, Kung-Fu Panda, How to Train Your Dragon, Madagascar, and The Boss Baby, and television series like Trollhunters, Spirit Riding Free, and All Hail King Julien, DreamWorks Animation is recognized across the globe as a leader in quality family entertainment.
+			</p>
+			</div>
+			<div class="box">
+			<h2 class="about-maker">About Maker</h2>
+			<p>
+				Thanks to movies like Shrek, Trolls, Kung-Fu Panda, How to Train Your Dragon, Madagascar, and The Boss Baby, and television series like Trollhunters, Spirit Riding Free, and All Hail King Julien, DreamWorks Animation is recognized across the globe as a leader in quality family entertainment.
+			</p>
+			</div>
+		</section>
+		<br>
+		<br>
+		<div>
+			This is Section F
+		</div>
+	</body>
+</html>
